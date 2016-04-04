f92a4d0f182ecb00d527c7bc927686adc18089d2
tony <zhangguof@gmail.com>
2016-04-05 00:12:44 +0800

    add camera project. make a  fps camera.

---
+++ camera/camera/cameraTest.hpp
@@ -0,0 +1,15 @@
+//
+//  camera.hpp
+//  camera
+//
+//  Created by tony on 16/4/4.
+//  Copyright © 2016年 tony. All rights reserved.
+//
+
+#ifndef cameraTest_hpp
+#define cameraTest_hpp
+
+#include <stdio.h>
+#include <gltools.hpp>
+
+#endif /* camera_hpp */
